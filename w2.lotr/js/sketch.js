0d3d36637d6a489714217c1b1f09b0a42890f412
Sagar Mohite <sagar.mohite@live.com>
2014-10-14 01:53:58 -0400

    changed w2lotr styles

--- w2.lotr/js/sketch.js
+++ w2.lotr/js/sketch.js
@@ -14,34 +14,39 @@ function generate() {
     return this.value;
   }).get();
 
-  // concat the above array separated by |
-  var OrString = characters.reduce(function(a,b){ return a + b + '|'; }, '');
-  OrString = OrString.substring(0, OrString.length - 1);
-
-
-  // var regex = new RegExp("^((?!.*?(" + OrString + ").*?).)*?$","gmi"); //Match all that don't contain __. Warning! Bad O(n).
-  var regex = new RegExp("^.*(" + OrString + ").*$","mi");
-
-  var results = regex.exec(lotr);
-
-  // remove all matching lines containing the OrStrings
-  while( results != null) {
-    lotr = lotr.replace(results[0], '');
-    results = regex.exec(lotr);
+  if(characters.length == 0) {
+    alert('Please select a character to remove');
   }
 
-  // HTMLize the plain text
-  regex = /Chapter \d+\n/mi;
-  results = regex.exec(lotr);
-  while(results != null){
-    console.log(results[0]);
-    lotr = lotr.replace(results[0], '<h2>'+ results[0] + '</h2>');
-    results = regex.exec(lotr);
+  else {
+    // concat the above array separated by |
+    var OrString = characters.reduce(function(a,b){ return a + b + '|'; }, '');
+    OrString = OrString.substring(0, OrString.length - 1);
+
+
+    // var regex = new RegExp("^((?!.*?(" + OrString + ").*?).)*?$","gmi"); //Match all that don't contain character. Warning! Bad O(n).
+    var regex = new RegExp("^.*(" + OrString + ").*$","mi"); // match all sentences that contain character
+
+    var results = regex.exec(lotr);
+
+    // remove all matching lines containing the OrStrings
+    while( results != null) {
+      lotr = lotr.replace(results[0], '');
+      results = regex.exec(lotr);
+    }
+
+    // HTMLize the plain text
+    // regex = /Chapter \d+\n/mi;
+    // results = regex.exec(lotr);
+    // while(results != null){
+    //   console.log(results[0]);
+    //   lotr = lotr.replace(results[0], '<h2>'+ results[0] + '</h2>');
+    //   results = regex.exec(lotr);
+    // }
+
+    lotr = lotr.replace(/\n+/g, "<br><br>");
+    // lotr = lotr.replace(/Chapter \d+/g, /<h2>\1<\/h2>/);
+    $('.out').append(lotr);
+    //$('.out').append("<br><br>");
   }
-
-  lotr = lotr.replace(/\n+/g, "<br><br>");
-  // lotr = lotr.replace(/Chapter \d+/g, /<h2>\1<\/h2>/);
-  $('.out').append(lotr);
-  //$('.out').append("<br><br>");
-
 }
